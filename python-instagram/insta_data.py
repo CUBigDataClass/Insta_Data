c178cfe2753a495224e5a8707f6d0cdb49f6c12e
EC2 Default User <ec2-user@ip-172-31-25-52.us-west-2.compute.internal>
2015-04-17 04:30:22 +0000

    setup python script to run

--- python-instagram/insta_data.py
+++ python-instagram/insta_data.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 import json
 
 connection = MongoClient('localhost', 27017)
-db = connection.insta_data1
+db = connection.insta_data
 
 
 session_opts = {
@@ -25,30 +25,37 @@ CONFIG = {
 api = client.InstagramAPI(access_token='4689265.35307b6.392bd0f97a694647baeded2c94e041b6')
 #media_popular = api.media_search(lat="37.7808851",lng="-122.3948632",distance=1000)
 media_popular = api.media_popular()
-
+i = 0
 # db[]
-
-for media in media_popular:
-	try:
+while(i < 10000):
+	for media in media_popular:
+		try:
 		# for m in media.tags:
 		# 	m.name
 		# print({"latitude":media.location.point.latitude,"longitude":media.location.point.longitude})
-		tags = [tag.name for tag in media.tags]
-		likes = media.like_count
-		url = media.get_standard_resolution_url()
-		location = {"latitude":media.location.point.latitude,"longitude":media.location.point.longitude}
+			tags = [tag.name for tag in media.tags]
+			likes = media.like_count
+			url = media.get_standard_resolution_url()
+			location = {}
+			try:
+				location = {"latitude":media.location.point.latitude,"longitude":media.location.point.longitude}
+			except Exception as e:
+				location = {"latitude": 0,"longitude": 0}
 			# print([method for method in dir(m) if callable(getattr(m, method))])
-			# print(v)
-		field = {"url":url, "likes":likes, "tag":tags, "location":location}
-		# asdf = {tag: count}
-		print(field)
-		db.posts.save(field)
-		# db.tags.
-		# print(media.get_standard_resolution_url())		#get image url
-		# print (media.like_count)		#get number of likes
-		# print (media.tags)		#get all tags in an array
-		# print (media.location)
+			username = media.user.username
+			postid = media.id
+			field = {"id":postid, "username":username, "imageurl":url, "likes":likes, "tag":tags, "location":location}
 
-	except Exception as e:
-		pass
-		# print( e)
+			# asdf = {tag: count}
+			# print(field)
+			db.posts.save(field)
+			i = i + 1
+			# db.tags.
+			# print(media.get_standard_resolution_url())		#get image url
+			# print (media.like_count)		#get number of likes
+			# print (media.tags)		#get all tags in an array
+			# print (media.location)
+
+		except Exception as e:
+			pass
+			# print( e)
